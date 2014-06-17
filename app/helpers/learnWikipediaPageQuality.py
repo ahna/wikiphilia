84a8742fc5c9ce93e0cdd01127cb0a7f4e0df72c
ahna <ahna.girshick@gmail.com>
2014-06-17 13:23:01 -0700

    added Random Forest

--- app/helpers/learnWikipediaPageQuality.py
+++ app/helpers/learnWikipediaPageQuality.py
@@ -12,12 +12,14 @@ import numpy as np
 import sklearn
 import sklearn.linear_model
 from sklearn.cross_validation import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 from sklearn import preprocessing
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import recall_score, precision_score
 import pickle
 #import qualityPredictor
-from app.helpers.qualityPredictor import qualPred
+#from app.helpers.qualityPredictor import qualPred
+from qualityPredictor import qualPred
 
 def optimizeRegConstant(logres):
     # determine regularization constant. TO DO: make it so it returns best C. also might need to save and restore original C
@@ -36,10 +38,10 @@ def main():
     
     ##############################################
     # file names
-    featured_csvfilename = '/Users/ahna/Documents/Work/insightdatascience/project/wikiphilia/data/featured.csv'
-    flagged_csvfilename = '/Users/ahna/Documents/Work/insightdatascience/project/wikiphilia/data/flagged.csv'
+    featured_csvfilename = '/Users/ahna/Documents/Work/insightdatascience/project/wikiphilia/webapp/datasets/featured.csv'
+    flagged_csvfilename = '/Users/ahna/Documents/Work/insightdatascience/project/wikiphilia/webapp/datasets/flagged.csv'
     #qualityPredictorFile = '/Users/ahna/Documents/Work/insightdatascience/project/wikiphilia/data/qualityPredictor.p'
-    qualityPredictorFile = '/Users/ahna/Documents/Work/insightdatascience/project/insightfl/app/helpers/qualityPredictorFile.p'
+    qualityPredictorFile = '/Users/ahna/Documents/Work/insightdatascience/project/wikiphilia/webapp/datasets/qualityPredictorFile.p'
         
     ##############################################
     # load data
@@ -53,50 +55,68 @@ def main():
     
     ##############################################
     # set up data matrices
-    # features are length, nextlinks, nimages, nlinks
-    X = DF[qp.featureNames].values.astype('float')
+    #useFeatures = [ 'meanSentLen','meanWordLength','medianSentLen','medianWordLength','nChars','nImages','nLinks','nRefs',\
+    #'nSections', 'nSents', 'nWordsSummary']
+#    useFeatures = ['meanWordLength','nImages','nLinks','nRefs','nSections','nSents', 'nWordsSummary']
+    DF = DF[~np.isnan(DF.meanWordLength)] # remove rows with NaN in meanWordLength
+    X = DF[qp.iUseFeatures].values #.astype('float')
     #X_scaled = preprocessing.scale(X) # feature normalization, save noralization info!
     
     # labels are 1 if featured is True, 0 if flagged is True
     y = DF['featured'].values
     
     ##############################################
-    
-    ##############################################
     # divide into training set and test set
     np.random.seed()    
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=qp.testPropor, train_size=1-qp.testPropor, random_state=np.random.random_integers(100))
+
+    ##############################################
+    qp.rfclf = RandomForestClassifier(n_estimators=10) #look at feature_importances_
+    qp.randfor = Pipeline([("scaler", preprocessing.StandardScaler()), ("clf", qp.rfclf)])
+    qp.randfor.fit(X_train, y_train)
+    print qp.randfor.score(X_test, y_test)
+    qp.rfclf.feature_importances_
+
         
     ##############################################
     # build learning pipeline for logistic regression
-    clf = sklearn.linear_model.LogisticRegression(C=0.35) 
-    qp.logres = Pipeline([("scaler", preprocessing.StandardScaler()), ("clf", clf)])
+    qp.logres_clf = sklearn.linear_model.LogisticRegression(C=0.35) 
+    qp.logres = Pipeline([("scaler", preprocessing.StandardScaler()), ("clf", qp.logres_clf)])
     qp.logres.fit(X_train, y_train)
-    qp.clf = clf
-#    qp.logres.score(X_test,y_test)
+    qp.logres.score(X_test,y_test)
 
     ##############################################
     # save results
     pickle.dump(qp,open(qualityPredictorFile, 'wb'))
-    print clf.raw_coef_
+    print qp.logres_clf.raw_coef_
     qp2 = pickle.load(open(qualityPredictorFile, 'rb'))
     print("Found: " + str(qp2))
 
     ##############################################
     # caculate accuracy on test set
-    print("Score on training data: " + str(qp.logres.score(X_train, y_train)))
-    print("Score on test data: " + str(qp.logres.score(X_test, y_test)))
+    print("Random forest score on training data: " + str(qp.randfor.score(X_train, y_train)))
+    print("Random forest score on test data: " + str(qp.randfor.score(X_test, y_test)))
+    preds = qp.randfor.predict(X_test)
+    print("Random forest Recall score on test data: " + str(recall_score(y_test, preds)))
+    print("Random forest sPrecision score on test data: " + str(precision_score(y_test, preds)))
+
+    print("Logistic regression Score on training data: " + str(qp.logres.score(X_train, y_train)))
+    print("Logistic regression Score on test data: " + str(qp.logres.score(X_test, y_test)))
     preds = qp.logres.predict(X_test)
-    print("Recall score on test data: " + str(recall_score(y_test, preds)))
-    print("Precision score on test data: " + str(precision_score(y_test, preds)))
+    print("Logistic regression Recall score on test data: " + str(recall_score(y_test, preds)))
+    print("Logistic regression Precision score on test data: " + str(precision_score(y_test, preds)))
     # Prec = 1, recall = 0.68 means no false negatives, erring on the side of labelling 1 (high quality)    
     
     ##############################################
     # determine which features matter and drop the ones that don't
+    print "Random forest feature importances:"
+    print qp.iUseFeatures
+    print qp.rfclf.feature_importances_
     
     ##############################################
     # deploy to unlabelled data
     # for final app, ideally i'd compute this index on all of wikipedia
+    #sklearn.linear_model.LogisticRegression.predict()    
     #sklearn.linear_model.LogisticRegression.predict()    
 
 
