8106b70161df1ea97c42d02b2af3a3f517cbbf6e
ahna <ahna.girshick@gmail.com>
2014-06-20 09:58:25 -0700

    cleaned up

--- app/helpers/database.py
+++ app/helpers/database.py
@@ -1,14 +1,25 @@
 import pymysql
 import sys
-
-
+#import app
+	
 # Returns MySQL database connection
-def con_db(host, port, user, passwd, db):
+def conDB(passwd='wikiscore123',host='localhost', port=3306, user='root', dbname='wikimeta'):
     try:
-        con = pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=db)
-
+        con = pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=dbname)
+        print("Opened database " + dbname)
+    
     except pymysql.Error, e:
         print "Error %d: %s" % (e.args[0], e.args[1])
         sys.exit(1)
 
     return con
+
+
+def curDB(conn):
+ 	return conn.cursor()
+	
+def closeDB(conn):
+	curDB(conn).close()
+	conn.commit()
+	conn.close()
+	print "Closed database " + conn.db
