42a534c19ad8a3226a3a89c2574668c5fd9c9aa0
ahna <ahna.girshick@gmail.com>
2014-06-22 16:18:26 -0700

    New scoring system with NLP

--- app/helpers/scrapeFeaturedWikipedia.py
+++ app/helpers/scrapeFeaturedWikipedia.py
@@ -14,13 +14,12 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import pickle
 #import pymysql
-import scrapeWikipedia
-scrapeWikipedia = reload(scrapeWikipedia)
+from scrapeWikipedia import wikiScraper
 
 
 def main():
 
-    conn, cur = scrapeWikipedia.openDB()
+#    conn, cur = scrapeWikipedia.openDB()
     
     csvfilename = '/Users/ahna/Documents/Work/insightdatascience/project/wikiphilia/webapp/datasets/featured.csv'
     linksfilename = '/Users/ahna/Documents/Work/insightdatascience/project/wikiphilia/webapp/datasets/featured.links.p'
@@ -33,7 +32,8 @@ def main():
          
     ################################################################
     # create a Wiki object
-    soup = scrapeWikipedia.grabWikiLinksFromURL(url)
+    ws = wikiScraper()
+    soup = ws.grabWikiLinksFromUrl(url)
     td = soup.findAll('td')[4] # the 5th table on page has all the links to the featured articles
     soup2 = BeautifulSoup(str(td),'xml')
     links = soup2.find_all("a")
@@ -42,42 +42,45 @@ def main():
     pickle.dump(str(links),open(linksfilename, 'wb'))
 
     # save wikipedia featured page meta data
-    featuredDF = scrapeWikipedia.getWikiPagesMeta(links,csvfilename,iStart=0,tablename='training')
+    #featuredDF = scrapeWikipedia.getWikiPagesMeta(links,csvfilename,iStart=0,tablename='training')
+    ws.getWikiPagesMeta(links=links,iStart=0,tablename='training2')        
         
     ################################################################
-    featuredDF['score'] = True # indicate that all these articles were featured
-    featuredDF['flagged'] = False # indicate that all these articles were not flagged
-    featuredDF['flags'] = 'NA' # indicate that there is no flag info
+    #featuredDF['score'] = True # indicate that all these articles were featured
+    #featuredDF['flagged'] = False # indicate that all these articles were not flagged
+    #featuredDF['flags'] = 'NA' # indicate that there is no flag info
     
     # save data frame to csv file
-    featuredDF.to_csv(csvfilename)
+    #featuredDF.to_csv(csvfilename)
 
     ################################################################
     # write to SQL database
-    iUse = ['meanWordLength','meanSentLength', 'nChars','nImages', 'nLinks','nSections', 'nSents',\
-    'nRefs', 'nWordsSummary','pageId','revisionId','title','url','score','flagged','flags']
-    import numpy as np
-    type_dict = {np.float64:float, np.int64:int, np.bool_:bool, str:str, float:float}
-    for i in range(0,len(featuredDF)):
-        print i
-        
-        row = list( featuredDF.ix[i,iUse])
-        # convert types
-        for j in range(len(row)):
-            row[j] = type_dict[type(row[j])](row[j])
-            if row[j] == 'NA' or (is_numlike(row[j]) and isnan(row[j])):
-                row[j] = None        
-         
-        # insert row into database
-        cur.execute('''INSERT INTO training (meanWordLength,meanSentLength, \
-        nChars,nImages, nLinks,nSections, nSents,nRefs, nWordsSummary,pageId,\
-        revisionId,title,url,score,flagged,flags) \
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''', row)
-        conn.commit()     
-             
-    cur.close()
-    conn.commit()
-    conn.close()
+#    iUse = ['meanWordLength','meanSentLength', 'nChars','nImages', 'nLinks','nSections', 'nSents',\
+#    'nRefs', 'nWordsSummary','pageId','revisionId','title','url','score','flagged','flags']
+#    import numpy as np
+#    type_dict = {np.float64:float, np.int64:int, np.bool_:bool, str:str, float:float}
+#    for i in range(0,len(featuredDF)):
+#        print i
+#        
+#        row = list( featuredDF.ix[i,iUse])
+#        # convert types
+#        for j in range(len(row)):
+#            row[j] = type_dict[type(row[j])](row[j])
+#            if row[j] == 'NA' or (is_numlike(row[j]) and isnan(row[j])):
+#                row[j] = None        
+#         
+#        # insert row into database
+#        cur.execute('''INSERT INTO training (meanWordLength,meanSentLength, \
+#        nChars,nImages, nLinks,nSections, nSents,nRefs, nWordsSummary,pageId,\
+#        revisionId,title,url,score,flagged,flags) \
+#        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''', row)
+#        conn.commit()     
+#             
+#    cur.close()
+#    conn.commit()
+#    conn.close()
 
 
-if __name__ == '__main__': main()
+if __name__ == '__main__': main():
+    
+# UPDATE training2 SET score = 1 WHERE score IS NULL;
