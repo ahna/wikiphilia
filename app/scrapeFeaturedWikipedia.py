9e0d387b5362dfae3c466b19be159e3df2d639b5
ahna <ahna.girshick@gmail.com>
2014-07-01 13:21:53 -0700

    cleaned up

--- app/scrapeFeaturedWikipedia.py
+++ app/scrapeFeaturedWikipedia.py
@@ -2,7 +2,7 @@
 
 """
 scrapeFeaturedWikipedia.py
-Grab list of featured articles from Wikipedia and saves to a CSV file
+Grab list of featured articles from Wikipedia, scrape features, and save to a database
 
 Created on Tue Jun 10 10:26:15 2014
 @author: ahna
@@ -11,24 +11,14 @@ Created on Tue Jun 10 10:26:15 2014
 ################################################################
 # imports
 from bs4 import BeautifulSoup
-import pandas as pd
 import pickle
-#import pymysql
 from scrapeWikipedia import wikiScraper
 
 
 def main():
 
-#    conn, cur = scrapeWikipedia.openDB()
-    
-    csvfilename = '/Users/ahna/Documents/Work/insightdatascience/project/wikiphilia/webapp/datasets/featured.csv'
     linksfilename = '/Users/ahna/Documents/Work/insightdatascience/project/wikiphilia/webapp/datasets/featured.links.p'
     url = "https://en.wikipedia.org/wiki/Wikipedia:Featured_articles"
-    # create a data frame
-#    featuredDF = pd.DataFrame(columns=['meanWordLength','meanSentLength', 'medianSentLength',  'medianWordLength', \
-#     'nChars','nImages', 'nLinks','nSections', 'nSents','nRefs', 'nWordsSummary','pageId',\
-#     'revisionId','title','url','score','flagged','flags'])
-     
          
     ################################################################
     # create a Wiki object
@@ -43,45 +33,7 @@ def main():
         pickle.dump(str(links),f)
 
     # save wikipedia featured page meta data
-    #featuredDF = scrapeWikipedia.getWikiPagesMeta(links,csvfilename,iStart=0,tablename='training')
     ws.getWikiPagesMeta(links=links,iStart=0,tablename='training2')        
-        
-    ################################################################
-    #featuredDF['score'] = True # indicate that all these articles were featured
-    #featuredDF['flagged'] = False # indicate that all these articles were not flagged
-    #featuredDF['flags'] = 'NA' # indicate that there is no flag info
+
+if __name__ == '__main__': main()
     
-    # save data frame to csv file
-    #featuredDF.to_csv(csvfilename)
-
-    ################################################################
-    # write to SQL database
-#    iUse = ['meanWordLength','meanSentLength', 'nChars','nImages', 'nLinks','nSections', 'nSents',\
-#    'nRefs', 'nWordsSummary','pageId','revisionId','title','url','score','flagged','flags']
-#    import numpy as np
-#    type_dict = {np.float64:float, np.int64:int, np.bool_:bool, str:str, float:float}
-#    for i in range(0,len(featuredDF)):
-#        print i
-#        
-#        row = list( featuredDF.ix[i,iUse])
-#        # convert types
-#        for j in range(len(row)):
-#            row[j] = type_dict[type(row[j])](row[j])
-#            if row[j] == 'NA' or (is_numlike(row[j]) and isnan(row[j])):
-#                row[j] = None        
-#         
-#        # insert row into database
-#        cur.execute('''INSERT INTO training (meanWordLength,meanSentLength, \
-#        nChars,nImages, nLinks,nSections, nSents,nRefs, nWordsSummary,pageId,\
-#        revisionId,title,url,score,flagged,flags) \
-#        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)''', row)
-#        conn.commit()     
-#             
-#    cur.close()
-#    conn.commit()
-#    conn.close()
-
-
-if __name__ == '__main__': main():
-    
-# UPDATE training2 SET score = 1 WHERE score IS NULL;
